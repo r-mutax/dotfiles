8e9f3e9f3e8c89bb20419a03f8f6a8eb71811640
r-mutax <magicspell.max.mix.mutax@gmail.com>
2023-07-15 17:05:15 +0900

    add symbolik_link to .bash_profile

--- .bash_profile
+++ .bash_profile
@@ -1,1 +1,6 @@
+export PATH=/usr/local/zig:$PATH
+if [[ -f ~/.bashrc ]] ; then
+	. ~/.bashrc
+fi
+
 alias emacs="emacs -nw"
--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# $HOME/dotfilesで実行されるはず
+
+# シンボリックリンクを張る
 ln -s $HOME/dotfiles/.emacs.d $HOME/.emacs.d
+ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
+
+# emacs向けの設定
+cd $HOME/.emacs.d
+#git clone https://github.com/dimitri/el-get.git
+mkdir lisp
+cd $HOME/dotfiles
+alias emacs='emacs -nw'
+
