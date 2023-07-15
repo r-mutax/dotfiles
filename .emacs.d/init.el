d178723ceba44ad3fb2c83ff82c8e2aa22c32bf8
r-mutax <magicspell.max.mix.mutax@gmail.com>
2023-07-15 23:53:39 +0900

    emacs: add undo-tree

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 ; init.el
-
+(setq byte-compile-warnings '(cl-functions))
 ;; ###########################################
 ;;  el-getの設定
 ;; ###########################################
@@ -74,8 +74,10 @@
   (selected-global-mode 1)
 )    
 
-
-
+;; undo-tree
+(el-get-bundle undo-tree)
+(global-undo-tree-mode t)
+(global-set-key (kbd "M-/") 'undo-tree-redo)
 
 ;; ###########################################
 ;; 見た目の設定
@@ -125,3 +127,15 @@
 ;; カレントの行を強調する
 (global-hl-line-mode t)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(queue)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
